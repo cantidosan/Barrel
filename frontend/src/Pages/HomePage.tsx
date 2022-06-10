32ad34481367599ad5aa7e4956a75afa818c9551
Cantidosan <82469261+cantidosan@users.noreply.github.com>
2022-06-10 12:13:09 -0400

    implmented user profile settings dropdown menu

--- frontend/src/Pages/HomePage.tsx
+++ frontend/src/Pages/HomePage.tsx
@@ -3,30 +3,32 @@ import React, { FC } from 'react'
 import prflag from '../assets/images/prflag.png'
 import americanflag from '../assets/images/americanflag.png'
 import RouteDetailsCardSm from '../components/RouteDetailsCardSm'
-
+import PageLayout from '../components/layouts/PageLayout'
 
 const HomePage: FC = () => {
 
     const url = [prflag, americanflag]
 
     return (
-        <div className='bg-barrel-green'>
-            <div className='container mx-auto pt-20 px-16 align-items-center '>
-                <div className=' grid grid-cols-3 bg-barrel-green gap-4  '>
+        <PageLayout>
+            <div className='bg-barrel-green'>
+                <div className='container mx-auto pt-20 px-16 align-items-center '>
+                    <div className=' grid grid-cols-3 bg-barrel-green gap-4  '>
 
-                    <RouteDetailsCardSm />
-                    <RouteDetailsCardSm />
-                    <RouteDetailsCardSm />
-                    <RouteDetailsCardSm />
-                    <RouteDetailsCardSm />
-                    <RouteDetailsCardSm />
-                    <RouteDetailsCardSm />
-                    <RouteDetailsCardSm />
-                    <RouteDetailsCardSm />
+                        <RouteDetailsCardSm />
+                        <RouteDetailsCardSm />
+                        <RouteDetailsCardSm />
+                        <RouteDetailsCardSm />
+                        <RouteDetailsCardSm />
+                        <RouteDetailsCardSm />
+                        <RouteDetailsCardSm />
+                        <RouteDetailsCardSm />
+                        <RouteDetailsCardSm />
 
-                </div>
+                    </div>
+                </div >
             </div >
-        </div >
+        </PageLayout>
     )
 }
 
