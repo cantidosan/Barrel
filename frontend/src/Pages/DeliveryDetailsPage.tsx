b5790b0b1cfab359b49d56df474e3380eafb2ecf
Cantidosan <82469261+cantidosan@users.noreply.github.com>
2022-06-16 11:35:21 -0400

    additional styling for delivery details page

--- frontend/src/Pages/DeliveryDetailsPage.tsx
+++ frontend/src/Pages/DeliveryDetailsPage.tsx
@@ -3,38 +3,72 @@ import PageLayout from '../components/layouts/PageLayout'
 import ParcelDeliveryDetails from '../components/ParcelDeliveryDetails'
 import DisplayDeliveryUpdates from '../components/DisplayDeliveryUpdates'
 import CreateDeliveryUpdates from '../components/CreateDeliveryUpdates'
+import DisplayRouteFlag from '../components/DisplayRouteFlag'
+import americanflag from '../assets/images/americanflag.png'
+import prflag from '../assets/images/prflag.png'
 
 
 const DeliveryDetailsPage: FC = () => {
+
+    const url = [prflag, americanflag]
+
+
     return (
         <PageLayout>
             <div className='bg-barrel-green'>
                 <main className=''>
-                    <section className='flex justify-center '>
+                    <section className='flex justify-center'>
                         <ParcelDeliveryDetails />
                     </section>
-                    <section className='flex justify-center  mt-8'>
-                        BLANK SECTION
+                    <section className='flex justify-center mt-8  bg-barrel-green '>
+                        <div className='grid grid-cols-2 flex-col border-2 gap-20'>
+                            <div className='flex border-2 bg-barrel-green'>
+                                <div className=' justify-center w-14' id="flags">
+                                    <DisplayRouteFlag url={url[1]} />
+                                    <DisplayRouteFlag url={url[0]} />
+                                </div>
+                                <div className=' ml-3' id="payouttext">
+                                    <span className='m-1 text-left font-bold font-roboto text-white opacity-60
+                                    text-md'>PAYOUT
+                                    </span><br />
+                                    <span className='text-right text-lg
+                                    font-bold text-white mr-1 text-lime-500'
+                                    >
+                                        $90.00
+                                    </span>
+                                </div>
+
+                            </div>
+
+                            <div className='border-2' id="porttext">
+                                <span className='m-1 text-left text-white opacity-60 font-bold font-roboto
+                                    text-md'>PORTS
+                                </span><br />
+                                <span className='text-right 
+                                    font-bold text-white mr-1'
+                                >
+                                    JFK -IATA
+                                </span><br />
+                                <span className='m-1 text-left font-bold font-roboto text-white opacity-60
+                                    text-sm'>EXCHANGE LOCATION
+                                </span><br />
+                                <span className='text-right 
+                                    font-bold text-white mr-1 text-lime-500'
+                                >
+                                    IATA arrivals
+                                </span>
+                            </div>
+
+
+
+                        </div>
                     </section>
-                    <section className='flex flex-cols gap-4 justify-center  mt-8'>
+                    <section className='flex flex-cols gap-4 
+                        justify-center mt-8'>
 
                         <DisplayDeliveryUpdates />
                         <CreateDeliveryUpdates />
-                        <div className='flex  '>
 
-
-
-                            <button id="scanButton" className=' text-white-500 bg-gradient-to-r 
-                                    from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br
-                                    focus:ring-4 focus:outline-none focus:ring-cyan-300
-                                    dark:focus:ring-cyan-800 shadow-lg shadow-cyan-500/50 
-                                    dark:shadow-lg dark:shadow-cyan-800/80 font-bold rounded-lg 
-                                    text-sm px-2.5  text-center opacity-70 hover:opacity-100 '
-                            >
-                                CANCEL
-                            </button>
-
-                        </div>
                     </section>
                 </main>
             </div>
