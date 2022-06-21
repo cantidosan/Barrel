6411c8ed201fd0fb4e5f91654149777fcafea2db
Cantidosan <82469261+cantidosan@users.noreply.github.com>
2022-06-21 11:38:18 -0400

    styled CreateRoutePage

--- frontend/src/Pages/CreateRoutePage.tsx
+++ frontend/src/Pages/CreateRoutePage.tsx
@@ -1,71 +1,102 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import PageLayout from '../components/layouts/PageLayout'
 import NavigationIcons from '../components/NavigationIcons'
 import Airports from '../assets/AirportList/Airports.json'
-import Datepicker from 'flowbite-datepicker/Datepicker';
+import Datepicker from 'react-datepicker'
+import "react-datepicker/dist/react-datepicker.css";
+import "react-widgets/styles.css";
+import DropdownList from "react-widgets/DropdownList"
+
+///###TODO ADD THE AIRPORT NAME AS PART OF THE DROPDOWN DISPLAY
+///###TODO We have the option of flight timetable look up API use however
+/// it does have a price limit
 
 const CreateRoutePage: FC = () => {
 
+    const [startDate, setStartDate] = useState(new Date());
+
+    const ApCodes: any = []
+
+    Airports.forEach(codePush)
+
+    function codePush(item: any, index: any, arr: any) {
+
+        ApCodes.push(item.code)
+
+    }
+
+    console.log(ApCodes[1])
     return (
+
         <PageLayout>
-            <div className='flex '> CreateRoutePage
 
+            <h2 className=" pt-3 flex justify-center 
+                bg-barrel-green underline
+                text-white font-hansief text-center"
+            >CREATE ROUTE </h2>
 
-                <select id="country" name="country">
-                    <option>Country</option>
-                    {Airports.map((airport, key) =>
-                        <option value={airport.country}>
-                            {airport.country}
-                        </option>
-                    )}
-
-                </select>
-                <select id="deptAirport" name="deptAirport">
-                    <option>Airport</option>
-                    {Airports.map((airport, key) =>
-                        <option value={airport.code}>
-                            {airport.name}
-                        </option>
-                    )}
-
-                </select>
-                <select id="arrivCountry" name="arrivCountry">
-                    <option>Country</option>
-                    {Airports.map((airport, key) =>
-                        <option value={airport.country}>
-                            {airport.country}
-                        </option>
-                    )}
-
-                </select>
-                <select id="arrivAirport" name="arrivAirport">
-                    <option>Airport</option>
-                    {Airports.map((airport, key) =>
-                        <option value={airport.code}>
-                            {airport.name}
-                        </option>
-                    )}
-
-                </select>
-                <div className="relative">
-                    <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                        <svg className="w-5 h-5 text-gray-500 dark:text-gray-400"
-                            fill="currentColor" viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg"><path
-                                fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
-                    </div>
-                    <input datepicker type="text" className="bg-gray-50 border border-gray-300 
-                        text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 
-                        focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 
-                        dark:border-gray-600 dark:placeholder-gray-400 dark:text-white 
-                        dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="Select date" />
-                </div>
-
-
-
-
+            <div className='p-2 bg-barrel-green'>
+                <NavigationIcons />
             </div>
+
+            <div className='flex justify-center bg-barrel-green'>
+                <section className='flex flex-col w-56  '>
+                    <span className='text-white font-bold pt-8'>FROM</span>
+                    <DropdownList defaultValue={'JFK'} data={ApCodes} />
+
+                    <span className='text-white font-bold pt-2 pb-2'>TO</span>
+                    <DropdownList defaultValue={'MIA'} data={ApCodes} />
+
+
+
+                </section>
+            </div>
+
+            <div className='flex justify-center bg-barrel-green'>
+                <section className='flex flex-col w-56'>
+
+                    <div className="mb-6 pt-2">
+                        <label htmlFor="email" className="block mb-2 text-md font-bold text-gray-900 dark:text-white">Flight #</label>
+                        <input type="email" id="email" className="bg-white-400 border border-gray-300 text-gray-900 text-sm rounded-lg 
+                            focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white-700 dark:border-gray-600 
+                            dark:placeholder-black-500 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="MIA256" required
+                        />
+                    </div>
+
+
+
+                </section>
+            </div>
+            <div className='grid justify-center p-3 bg-barrel-green' >
+                <span className='text-white font-bold' >Departure Date</span>
+                <Datepicker selected={startDate} onChange={(date: Date) => setStartDate(date)} />
+            </div>
+            {
+
+
+                <div className='flex justify-center w-58 bg-barrel-green'>
+                    <button className='m-2 focus:outline-none text-white bg-purple-700 
+                        hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg 
+                        text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900'
+                        type="submit"
+                    >
+                        NEXT
+                    </button>
+                    <button className='m-2 focus:outline-none text-white bg-purple-700 
+                        hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium 
+                        rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 
+                        dark:focus:ring-purple-900"' type="submit"
+                    >
+                        CANCEL
+                    </button>
+                </div>
+            }
+
+
+
+
+
         </PageLayout>
     )
 }
