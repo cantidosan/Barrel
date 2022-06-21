e998e8ba2b515172bf8392b0505df2ca25a34d16
Cantidosan <82469261+cantidosan@users.noreply.github.com>
2022-06-21 12:54:20 -0400

    styled input luggage details comp

--- frontend/src/components/InputLuggageDetails.tsx
+++ frontend/src/components/InputLuggageDetails.tsx
@@ -2,11 +2,12 @@ import React, { FC } from 'react'
 
 const InputLuggageDetails: FC = () => {
     return (
-        <div className='grid justify-center bg-barrel-green text-white font-bold p-3 underline gap-3 font-hansief '>
-            LuggageDetails
+        <div className='grid justify-center bg-barrel-green text-white font-bold p-3 
+            underline gap-3 font-hansief '>
+            <span className='text-center'>LuggageDetails</span>
             <label htmlFor="green-toggle"
 
-                className=" inline-flex relative items-center 
+                className="ml-8 inline-flex relative 
                     mr-5 cursor-pointer"
             >
 
@@ -27,10 +28,33 @@ const InputLuggageDetails: FC = () => {
                 </div>
 
             </label>
-            <section>
+
+            <section className=''>
+                <div>
+                    <label htmlFor="item_length" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">WEIGHT</label>
+                    <input type="text" id="item_length" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 
+                        block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="lbs" required />
+                </div>
 
             </section>
-            <section>
+            <h2 className='flex justify-center'>SPACE</h2>
+            <section className=''>
+                <div>
+                    <label htmlFor="item_length" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Length</label>
+                    <input type="text" id="item_length" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 
+                        block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="feet" required />
+                </div>
+                <div>
+                    <label htmlFor="item_width" className=" pt-2 block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Width</label>
+                    <input type="text" id="item_width" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 
+                        block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="feet" required />
+                </div>
+                <div>
+                    <label htmlFor="item_height" className="pt-2 block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Height</label>
+                    <input type="text" id="item_height" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 
+                        block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="feet" required />
+                </div>
+
 
             </section>
         </div>
