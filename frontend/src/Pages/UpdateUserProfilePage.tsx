ff2935a072d37f3c53e89b1aad5a5b40e35bb3c7
Cantidosan <82469261+cantidosan@users.noreply.github.com>
2022-06-24 12:42:41 -0400

    refactored UpdateUserProfilePage styling

--- frontend/src/Pages/UpdateUserProfilePage.tsx
+++ frontend/src/Pages/UpdateUserProfilePage.tsx
@@ -71,8 +71,21 @@ const UpdateUserProfilePage: FC = () => {
             >
                 {
                     pageCount === 0 ? <UpdateUserProfileInput /> :
-                        pageCount === 1 ? <div > <UploadImage /></div> :
-                            pageCount === 2 ? <UploadImage /> : ''
+                        pageCount === 1 ? <UploadImage /> :
+                            pageCount === 2 ? <div><UploadImage /><label htmlFor="passpNumber" className='block mb-2 text-sm font-medium 
+                            text-gray-900 dark:text-gray-300'
+                            >
+                                Passport #
+                            </label>
+
+                                <input id="passpNumber"
+                                    name="passpNumber"
+                                    placeholder=" #453320498"
+                                    className="block p-2 w-full text-gray-900 bg-gray-50 rounded-lg 
+                                        border border-gray-300 sm:text-xs focus:ring-blue-500 focus:border-blue-500 
+                                        dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white 
+                                        dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                /></div> : ''
                 }
 
 
