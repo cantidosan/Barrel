d555f5061c51a0ddd0422a7911355666f8575e57
Cantidosan <82469261+cantidosan@users.noreply.github.com>
2022-06-24 13:04:22 -0400

    corrected of center header component

--- frontend/src/components/layouts/Header/Header.tsx
+++ frontend/src/components/layouts/Header/Header.tsx
@@ -17,18 +17,22 @@ const Header: FC<any> = (): ReactElement => {
         <nav className="bg-white border-b-2 border-gray-500 px-2 sm:px-4 py-2.5 rounded dark:bg-barrel-green"  >
             <div className="container flex border-gray-300  flex-wrap justify-between items-center mx-auto">
                 <a href="#" className="flex items-center">
-                    <img src={headerLogo} className="h-6 sm:h-9" alt="Barrel Logo" />
+                    <img src={headerLogo}
+                        className="h-6 sm:h-9"
+                        alt="Barrel Logo" />
                 </a>
                 <div className="flex items-center md:order-2">
-                    <p className=' mr-10 font-hansief text-gray-100'>BECOME A CoURIER</p>
-                    <button type="button" className="flex mr-3 text-sm bg-gray-800  
+                    <span className=' mr-10 font-hansief text-gray-100'>BECOME A CoURIER</span>
+                    <button type="button"
+                        className="flex mr-3 text-sm bg-gray-800  
                         rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 
                         ark:focus:ring-gray-600" id="user-menu-button"
                         aria-expanded="false"
                         data-dropdown-toggle="dropdown"
                     >
                         <span className="sr-only">Open user menu</span>
-                        <img className="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-3.jpg"
+                        <img className="w-8 h-8 rounded-full"
+                            src="/docs/images/people/profile-picture-3.jpg"
                             alt="user photo" />
                     </button>
 
@@ -41,16 +45,20 @@ const Header: FC<any> = (): ReactElement => {
                         </div>
                         <ul className="py-1" id="dropdown">
                             <li>
-                                <a href="#" className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Dashboard</a>
+                                <a href="#"
+                                    className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Dashboard</a>
                             </li>
                             <li>
-                                <a href="#" className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Settings</a>
+                                <a href="#"
+                                    className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Settings</a>
                             </li>
                             <li>
-                                <a href="#" className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Earnings</a>
+                                <a href="#"
+                                    className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Earnings</a>
                             </li>
                             <li>
-                                <a href="#" className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
+                                <a href="#"
+                                    className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
                             </li>
                         </ul>
                     </div>
@@ -63,12 +71,22 @@ const Header: FC<any> = (): ReactElement => {
                         aria-controls="mobile-menu-2" aria-expanded="false"
                     >
                         <span className="sr-only">Open main menu</span>
-                        <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
-                        <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+                        <svg className="w-6 h-6"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path fillRule="evenodd"
+                                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
+                        <svg className="hidden w-6 h-6"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path fillRule="evenodd"
+                                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                     </button>
                 </div>
                 <div className="hidden justify-between items-center w-full md:flex md:w-auto md:order-1" id="mobile-menu-2">
-                    <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium font-hansief">
+                    <ul className="flex flex-col pl-28  mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium font-hansief">
 
                         <li>
                             <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">WHERE</a>
