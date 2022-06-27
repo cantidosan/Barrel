8abc1e83184e0046715d3a58498a7211d054cef2
Cantidosan <82469261+cantidosan@users.noreply.github.com>
2022-06-27 10:06:44 -0400

    added conditional styling

--- frontend/src/components/BidDetails.tsx
+++ frontend/src/components/BidDetails.tsx
@@ -1,17 +1,52 @@
 import React, { FC } from 'react'
 
 const BidDetails: FC = () => {
+
+    ///THIS FUNCTION CONDITIONALLY RENDERS BASED ON THE STATE OF THE ROUTE
+    let emptyBool = false
+
     return (
-        <div className='flex flex-col p-3'>
-            <div className=''>
-                <span className='text-white font-thin opacity-80 font-roboto'>WEIGHT:</span><br />
-                <span className=' font-bold text-white'>13 lbs</span><br />
-            </div>
-            <div className=''>
-                <span className='text-white font-thin opacity-80 font-roboto'>SPACE:</span><br />
-                <span className=' font-bold text-white'>15 Sqft   </span><br />
-            </div>
-        </div>
+        <>
+            {!!emptyBool ?
+                <div className='flex flex-col p-3'>
+                    <div className=''>
+                        <span className='text-white font-thin opacity-80 font-roboto'>WEIGHT:</span><br />
+                        <span className=' font-bold text-white'>13 lbs</span><br />
+                    </div>
+                    <div className=''>
+                        <span className='text-white font-thin opacity-80 font-roboto'>SPACE:</span><br />
+                        <span className=' font-bold text-white'>15 Sqft   </span><br />
+                    </div>
+                </div > : ''
+            }
+            {!!emptyBool ? '' :
+                <div>
+                    <div className='md:pt-4'>
+                        <span className='text-white text-xs md:text-lg md:text-xl font-thin opacity-80 font-roboto underline pl-5'>WEIGHT:</span><br />
+
+                        <span className='font-bold text-xs md:text-lg md:text-xl text-white '>TOTAL </span>
+                        <span className='ml-3 font-bold text-xs md:text-lg md:text-xl text-white'>USED </span><br />
+                        <span className='ml-2 font-bold text-xs md:text-lg md:text-xl text-white'>13 </span>
+
+                        <span className='ml-8 font-bold text-xs md:text-lg md:text-xl text-lime-500'>8.5</span>
+                        <span className=' ml-1 text-2xs  text-white opacity-60'>lbs</span>
+
+                    </div>
+                    <div className='md:pt-4'>
+                        <span className='text-white text-xs md:text-lg md:text-xl font-thin opacity-80 font-roboto pl-5 underline'>SPACE:</span><br />
+                        <span className='font-bold text-xs md:text-lg md:text-xl text-white '>TOTAL </span>
+                        <span className='ml-3 font-bold text-xs md:text-lg md:text-xl text-white'>USED </span><br />
+                        <span className='ml-2 font-bold text-xs md:text-lg md:text-xl text-white'>5 </span>
+
+                        <span className='ml-8 font-bold text-xs md:text-lg md:text-xl text-lime-500'>2.5</span>
+                        <span className=' ml-1 text-2xs  text-white opacity-60'>sqft</span>
+
+                    </div>
+                </div>
+            }
+        </>
+
+
     )
 }
 
--- frontend/src/components/DisplayBidDetails.tsx
+++ frontend/src/components/DisplayBidDetails.tsx
@@ -4,8 +4,11 @@ import DecideBid from './DecideBid'
 import MakeBid from './MakeBid'
 
 const DisplayBidDetails: FC = () => {
+
+    let emptyBool = false
+
     return (
-        <div className='flex flex-row  '>
+        <div className='flex flex-row border  '>
             {true ? <MakeBid /> : <DecideBid />}
             <BidDetails />
         </div>
