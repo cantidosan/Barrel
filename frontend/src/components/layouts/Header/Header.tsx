607aebf58f9cc7c59c6fdd099f0d1b6053063c6a
Cantidosan <82469261+cantidosan@users.noreply.github.com>
2022-06-28 11:57:25 -0400

    barrel logo now links to homepage across app

--- frontend/src/components/layouts/Header/Header.tsx
+++ frontend/src/components/layouts/Header/Header.tsx
@@ -21,7 +21,7 @@ const Header: FC<any> = (): ReactElement => {
 
         <nav className="bg-white border-b-2 border-gray-500 px-2 sm:px-4 py-2.5 rounded dark:bg-barrel-green"  >
             <div className="container flex border-gray-300  flex-wrap justify-between items-center mx-auto">
-                <a href="#" className="flex items-center">
+                <a href="/" className="flex items-center">
                     <img src={headerLogo}
                         className="h-6 sm:h-9"
                         alt="Barrel Logo" />
