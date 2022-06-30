c5b0eadf4abe18aef2eb1ed946ae7986213dc8c6
Cantidosan <82469261+cantidosan@users.noreply.github.com>
2022-06-30 09:13:39 -0400

    refactored the center of theh header for cleaner code

--- frontend/src/components/layouts/Header/HeaderMain.tsx
+++ frontend/src/components/layouts/Header/HeaderMain.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import headerLogo from '../../../assets/images/headerLogo.png'
+import HeaderCenterCTA from './HeaderCenterCTA'
+
 
 function HeaderMain() {
     return (
@@ -88,19 +90,7 @@ function HeaderMain() {
                     </button>
                 </div>
                 <div className="hidden justify-between items-center w-full md:flex md:w-auto md:order-1" id="mobile-menu-2">
-                    <ul className="flex flex-col pl-28  mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium font-hansief">
-
-                        <li>
-                            <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">WHERE</a>
-                        </li>
-                        <li>
-                            <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">WHAT</a>
-                        </li>
-                        <li>
-                            <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">WHEN</a>
-                        </li>
-
-                    </ul>
+                    <HeaderCenterCTA />
                 </div>
             </div>
         </nav >
