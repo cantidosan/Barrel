f2ffba52fa736f47f72257be62063be517be308b
Cantidosan <82469261+cantidosan@users.noreply.github.com>
2022-07-12 13:07:53 -0400

    enables completed route creation and db storage

--- frontend/src/components/InputExchangeDetails.tsx
+++ frontend/src/components/InputExchangeDetails.tsx
@@ -1,3 +1,4 @@
+import { setDoc } from 'firebase/firestore';
 import React, { FC,useState } from 'react'
 import { useStateValue } from '../state'
 
@@ -13,6 +14,11 @@ const InputExchangeDetails: FC = () => {
 
     const handleClick = async () => {
 
+        await setDoc(docRef, {
+            exchange_location: exLocation,
+            exchange_policy:exPolicy
+        }, { merge: true })
+        
         //switches to part to of the form input
         pgContext++
         // console.log('PageCount+', pgContext)
@@ -22,7 +28,7 @@ const InputExchangeDetails: FC = () => {
         }
         //TODO use firebase SDK docRef and local variables to
         //update the route in the DB
-
+        
         dispatch({ type: 'pageCount', payload: pgContext });
     }
     return (
@@ -40,7 +46,7 @@ const InputExchangeDetails: FC = () => {
                 <textarea id="message" rows={4} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 
                     focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         placeholder="Leave a comment..."
-                        onChange={(ev)=>setExLocation(ev.target.value)}>
+                        onChange={(ev)=>setExPolicy(ev.target.value)}>
                         
                 </textarea>
             </div>
--- frontend/src/components/InputLuggageDetails.tsx
+++ frontend/src/components/InputLuggageDetails.tsx
@@ -1,20 +1,58 @@
 import React, { FC, useEffect, useState } from 'react'
 import ToggleMetricsButton from '../buttons/ToggleMetricsButton'
 import { useStateValue } from '../state/';
-
+import { doc, setDoc,getFirestore,collection,addDoc } from "firebase/firestore"; 
+import { firebaseConfig } from "../FirebaseConfig";
+import { initializeApp } from "firebase/app";
 
 const InputLuggageDetails: FC = () => {
 
-    const [{ pageCount, metricToggle }, dispatch] = useStateValue();
 
+    const app = initializeApp(firebaseConfig);
+    const db = getFirestore(app);
+
+    const [{ pageCount, metricToggle, docRef }, dispatch] = useStateValue();
+    
+    //
+    // const luggageRef = doc(db, 'routes', `${docRef.id}`);
+    // const luggageRef = db.collection("routes").doc(`${docRef.id}`)
+    const luggageRef =collection(db, "routes", docRef.id, "luggage")
+    
+
+    
+    // console.log('lugRef',luggageRef)
     const [weightMetric, setWeightMetric] = useState('lbs');
     const [sizeMetric, setSizeMetric] = useState('feet');
+    const [luggageWeight, setLuggageWeight] = useState('');
+    const [luggageHeight, setLuggageHeight] = useState('');
+    const [luggageWidth, setLuggageWidth] = useState('');
+    const [luggageLength, setLuggageLength] = useState('');
+
+    let pgContext = pageCount
 
 
-    console.log('sizemetric', sizeMetric)
-    console.log('weightmetric', weightMetric)
+    const handleClick = async () => {
+
+        await addDoc(luggageRef, {
+            height_capacity: luggageHeight,
+            length_capacity:luggageLength,
+            weight_capacity: luggageWeight,
+            width_capacity:luggageWidth
+        })
 
 
+         //switches to part to of the form input
+         pgContext++
+         // console.log('PageCount+', pgContext)
+ 
+         if (pgContext > 2) {
+             pgContext = 0
+         }
+ 
+         dispatch({ type: 'pageCount', payload: pgContext });
+
+
+    }
     useEffect(() => {
 
         if (metricToggle === 'MET') {
@@ -44,6 +82,7 @@ const InputLuggageDetails: FC = () => {
                         block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 
                         dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         placeholder={weightMetric} required
+                        onChange={(e)=>setLuggageWeight(e.target.value)}
                     />
                 </div>
 
@@ -54,20 +93,36 @@ const InputLuggageDetails: FC = () => {
                     <label htmlFor="item_length" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Length</label>
                     <input type="text" id="item_length" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 
                         block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder={sizeMetric} required />
+                        placeholder={sizeMetric} required
+                        onChange={(ev)=>setLuggageLength(ev.target.value)}
+                    />
                 </div>
                 <div>
                     <label htmlFor="item_width" className=" pt-2 block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Width</label>
                     <input type="text" id="item_width" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 
                         block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder={sizeMetric} required />
+                        placeholder={sizeMetric} required
+                        onChange={(ev)=>setLuggageWidth(ev.target.value)}
+                    />
                 </div>
                 <div>
                     <label htmlFor="item_height" className="pt-2 block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Height</label>
                     <input type="text" id="item_height" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 
                         block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder={sizeMetric} required />
+                        placeholder={sizeMetric} required
+                        onChange={(eve)=>setLuggageHeight(eve.target.value)}
+                    />
                 </div>
+                <div className='flex justify-center w-58 bg-barrel-green pt-5'>
+                <button className='m-2 focus:outline-none text-white bg-purple-700 
+                    hover:bg-purple-800 focus:ring-4 focus:ring-purple-300
+                    font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600
+                    dark: hover: bg-purple-700 dark:focus:ring-purple-900'
+                type="submit" onClick={handleClick}
+                >
+                NEXT
+                </button>
+            </div>
 
 
             </section>
