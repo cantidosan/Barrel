481961b8dc9caa1f5daa1e4987bef88c807b04c0
Cantidosan <82469261+cantidosan@users.noreply.github.com>
2022-08-03 09:49:40 -0400

    allows component to read parcelID from context

--- frontend/src/Pages/RouteDetailsPage.tsx
+++ frontend/src/Pages/RouteDetailsPage.tsx
@@ -149,7 +149,7 @@ const RouteDetailsPage: FC = () => {
                                 <>
                                 <ParcelPictures userId={user.uid} />
                                 <NewItemButton/>
-                                </>
+                                </> 
                                 : ''
                             
                         }
--- frontend/src/components/ParcelDeliveryDetails.tsx
+++ frontend/src/components/ParcelDeliveryDetails.tsx
@@ -1,19 +1,63 @@
-import React, { FC } from 'react'
+import React, { FC, useState,useEffect } from 'react'
 import americanflag from '../assets/images/americanflag.png'
 import prflag from '../assets/images/prflag.png'
 import ParcelDeliveryInfo from './ParcelDeliveryInfo'
 import { useStateValue } from '../state/index'
-
-
+import {
+    getFirestore, addDoc, updateDoc,
+    doc, getDocs, query,
+    collection, where, getDoc
+} from "firebase/firestore"; 
+import { initializeApp } from "firebase/app";
+import { firebaseConfig } from "../FirebaseConfig";
 
 const ParcelDeliveryDetails: FC = () => {
 
-    const url = [prflag, americanflag]
-
+    
+    const [itemUrl,setItemUrl]=useState('')
     //The component will be listen for parcelId 
     const [{ parcelId }, dispatch] = useStateValue();
+    const app = initializeApp(firebaseConfig);
+    const db = getFirestore(app);
 
-    !!parcelId ? url[1] = parcelId : url[1] = prflag
+    
+
+    useEffect(() => {
+        
+        if(parcelId !== ''){
+        const docRef = doc(db, "items", parcelId as string);
+    
+        getDoc(docRef).then(docSnap => {
+
+            if (docSnap.exists()) {
+               console.log(docSnap.data())
+                
+               getDocs(query(collection(db, `items/${parcelId}/pictures`),
+               where('url', '!=', '')))
+               .then((querySnapshot) => {
+                   // console.log("picIDSnapshot", querySnapshot)
+                   querySnapshot.forEach((result) => {
+
+                      setItemUrl(result.data().url)
+                   
+                   }
+                   )
+
+               }
+           )
+
+                console.log("Document data: critical", docSnap.data())
+                    ;
+
+                } else {
+                // doc.data() will be undefined in this case
+                console.log("No such document!");
+                }
+        })
+    }
+    }, [parcelId])
+
+    console.log('itemURl',itemUrl)
 
     return (
         <div className='flex flex-cols items-center'>
@@ -23,7 +67,7 @@ const ParcelDeliveryDetails: FC = () => {
                 <img className=" 
                     block object-fit 
                     object-center  rounded-lg"
-                    src={url[1]} alt="" width="168"
+                    src={itemUrl} alt="" width="168"
                 />
                 <ParcelDeliveryInfo />
             </figure>
